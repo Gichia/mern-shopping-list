9c8fc79a38e73f53dd254dd7bd981db2629c4057
Gichia <petergichia35@gmail.com>
2019-05-20 13:00:45 +0300

    Adds the delete item endpoint

--- routes/api/items.js
+++ routes/api/items.js
@@ -11,6 +11,7 @@ router.get('/', (req, res) => {
   Item.find()
     .sort({ date: -1 })
     .then(items => res.json(items))
+    
 });
 
 // @route POST api/items
@@ -23,8 +24,24 @@ router.post('/', (req, res) => {
 
   newItem
     .save()
-    .then(item => res.json(item));
+    .then(item => res.status(201).json(item));
+
 });
 
+// @route DELETE api/items/1
+// @desc Delete An Item
+// @access Public
+router.delete('/:id', (req, res) => {
+  Item.findById(req.params.id)
+    .then(item => item.remove().then(() => {
+      res.json({
+        message: "Item deleted!"
+      })
+    }))
+    .catch(err => res.status(404).json({
+      message: "Could not find item with that ID!"
+    }));
+
+});
 
 module.exports = router;
